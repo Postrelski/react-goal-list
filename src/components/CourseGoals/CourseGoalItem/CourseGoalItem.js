598aaa9619fea43b920ae949f5706dab02a69580
Matthew Postrel <matthewpostrel@gmail.com>
2023-08-28 17:52:16 -0400

    first commit

---
+++ src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./CourseGoalItem.css";
+
+const CourseGoalItem = (props) => {
+  const deleteHandler = () => {
+    props.onDelete(props.id);
+  };
+
+  return (
+    <li className="goal-item" onClick={deleteHandler}>
+      {props.children}
+    </li>
+  );
+};
+
+export default CourseGoalItem;
+
+// props.children = special prop used to pass data from parent to children component
